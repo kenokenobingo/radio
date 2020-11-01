fc5727d07cadb57152c339c4bfcdbdfdbf33f209
kenokenobingo <mail.kewe@gmail.com>
2020-11-01 10:50:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+Amplitude.init({
+    "bindings": {
+      37: 'prev',
+      39: 'next',
+      32: 'play_pause'
+    },
+    "songs": [
+      {
+        "name": "bauhaus.fm",
+        "url": "http://bauhaus.fm:8000/_b",
+        "cover_art_url": "https://www.uni-weimar.de/projekte/bauhaus-fm/wp-content/uploads/2016/10/logo-header.png"
+      }
+    ]
+  });
+
+  window.onkeydown = function(e) {
+      return !(e.keyCode == 32);
+  };
+
+  /*
+    Handles a click on the song played progress bar.
+  */
+  document.getElementById('song-played-progress').addEventListener('click', function( e ){
+    var offset = this.getBoundingClientRect();
+    var x = e.pageX - offset.left;
+
+    Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+  });
